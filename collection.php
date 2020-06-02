ddc1a807e243c5033392e6ea46a2d9ff9ea91040
hunglephu <hunglephu>
2020-06-03 05:58:50 +0700

    tam thoi nhu the, van~ con` 1 so cho~ chua ok, nhung tam on, fix tai master

---
+++ collection.php
@@ -0,0 +1,127 @@
+<?php include("header.html") ?>
+
+<div class="main-banner" style="width: 100%;">
+        <span class="mainBanner__link__content">
+              <!-- <h1 style="font-size: 70px; color: #ffffff;"><a href="#" style="text-decoration: none; font-family: auto;color: #ffffff;">Your day, Sorted</a></h1> -->
+              <br>
+              <!-- <button type="button" class="btn btn-light" style="letter-spacing: 4px; font-size: 12px;">SHOP THE RANGE</button> -->
+        </span>
+      </div>
+<div class="container">
+    <div class="row wrapper">
+        <div class="col-6"><b>Home / iPhone 11 Cases</b></div>
+        <div class="col-6"></div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-3 items-collection">
+            <div class="collection-item-img">
+                <img src="image/4.jpeg" style="width: 100%; height:100%" alt="">
+            </div>
+            <div class="collection-color-item"></div>
+            <div class="collection-product-price">
+                <span><b>Note Sleeve</b>
+                    <br> <b>89$</b>
+                    <br> 4 - 11+ card, flat bills, coins
+                </span>
+            </div>
+        </div>
+        
+        <div class="col-3 items-collection">
+            <div class="collection-item-img">
+                <img src="image/4.jpeg" style="width: 100%; height:100%" alt="">
+            </div>
+            <div class="collection-color-item"></div>
+            <div class="collection-product-price">
+                <span><b>Note Sleeve</b>
+                    <br> <b>89$</b>
+                    <br> 4 - 11+ card, flat bills, coins
+                </span>
+            </div>
+        </div>
+
+        <div class="col-3 items-collection">
+            <div class="collection-item-img">
+                <img src="image/4.jpeg" style="width: 100%; height:100%" alt="">
+            </div>
+            <div class="collection-color-item"></div>
+            <div class="collection-product-price">
+                <span><b>Note Sleeve</b>
+                    <br> <b>89$</b>
+                    <br> 4 - 11+ card, flat bills, coins
+                </span>
+            </div>
+        </div>
+
+        <div class="col-3 items-collection">
+            <div class="collection-item-img">
+                <img src="image/4.jpeg" style="width: 100%; height:100%" alt="">
+            </div>
+            <div class="collection-color-item"></div>
+            <div class="collection-product-price">
+                <span><b>Note Sleeve</b>
+                    <br> <b>89$</b>
+                    <br> 4 - 11+ card, flat bills, coins
+                </span>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-3 items-collection">
+            <div class="collection-item-img">
+                <img src="image/4.jpeg" style="width: 100%; height:100%" alt="">
+            </div>
+            <div class="collection-color-item"></div>
+            <div class="collection-product-price">
+                <span><b>Note Sleeve</b>
+                    <br> <b>89$</b>
+                    <br> 4 - 11+ card, flat bills, coins
+                </span>
+            </div>
+        </div>
+        
+        <div class="col-3 items-collection">
+            <div class="collection-item-img">
+                <img src="image/4.jpeg" style="width: 100%; height:100%" alt="">
+            </div>
+            <div class="collection-color-item"></div>
+            <div class="collection-product-price">
+                <span><b>Note Sleeve</b>
+                    <br> <b>89$</b>
+                    <br> 4 - 11+ card, flat bills, coins
+                </span>
+            </div>
+        </div>
+
+        <div class="col-3 items-collection">
+            <div class="collection-item-img">
+                <img src="image/4.jpeg" style="width: 100%; height:100%" alt="">
+            </div>
+            <div class="collection-color-item"></div>
+            <div class="collection-product-price">
+                <span><b>Note Sleeve</b>
+                    <br> <b>89$</b>
+                    <br> 4 - 11+ card, flat bills, coins
+                </span>
+            </div>
+        </div>
+
+        <div class="col-3 items-collection">
+            <div class="collection-item-img">
+                <img src="image/4.jpeg" style="width: 100%; height:100%" alt="">
+            </div>
+            <div class="collection-color-item"></div>
+            <div class="collection-product-price">
+                <span><b>Note Sleeve</b>
+                    <br> <b>89$</b>
+                    <br> 4 - 11+ card, flat bills, coins
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php include("footer.html") ?>
